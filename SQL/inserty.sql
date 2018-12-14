26675da1ce08545c8dee32c18a0cc79588a4e991
st52531 <43739550+st52531@users.noreply.github.com>
2018-12-14 20:32:26 +0100

    přidán insert, sekvence a trigger STUD_PLAN

---
+++ SQL/inserty.sql
@@ -0,0 +1,228 @@
+/*Insert Fakulta*/
+INSERT INTO FAKULTA (ZKRATKA_FAKULTY, NAZEV_FAKULTY) 
+VALUES ('DFJ', N'Dopravní fakulta Jana Pernera');
+
+INSERT INTO FAKULTA (ZKRATKA_FAKULTY, NAZEV_FAKULTY) 
+VALUES ('FEI', N'Fakulta elektrotechniky a informatiky');
+
+INSERT INTO FAKULTA (ZKRATKA_FAKULTY, NAZEV_FAKULTY) 
+VALUES ('FES', N'Fakulta ekonomicko-správní');
+
+INSERT INTO FAKULTA (ZKRATKA_FAKULTY, NAZEV_FAKULTY) 
+VALUES ('FF', N'Fakulta filozofická');
+
+INSERT INTO FAKULTA (ZKRATKA_FAKULTY, NAZEV_FAKULTY) 
+VALUES ('FCH', N'Fakulta chemicko-technologická');
+
+INSERT INTO FAKULTA (ZKRATKA_FAKULTY, NAZEV_FAKULTY) 
+VALUES ('FR', N'Fakulta restaurování');
+
+INSERT INTO FAKULTA (ZKRATKA_FAKULTY, NAZEV_FAKULTY) 
+VALUES ('FZS', N'Fakulta zdravotnických studií');
+
+/*Insert Katedra*/
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('ARK', N'Ateliér restaurování kamene', 'FR');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('ARMS', N'Ateliér restaurování malby a sgrafita', 'FR');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('ARPKVD', N'Ateliér restaurování papíru, knižní vazby a dokumentů', 'FR');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('ARUDP', N'Ateliér restaurování uměleckých děl na papíru', 'FR');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('AVP', N'Ateliér výtvarné přípravy', 'FR');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('CEMNAT', N'Centrum materiálů a nanotechnologií', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('JC', N'Jazykové centrum', 'REK');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KAA', N'Katedra anglistiky a amerikanistiky', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KALCH', N'Katedra analytické chemie', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KANT', N'Katedra anorganické technologie', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KBBV', N'Katedra biologických a biochemických věd', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KCJ', N'Katedra cizích jazyků', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KDMML', N'Katedra dopravního managementu, marketingu a logistiky', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KDPD', N'Katedra dopravních prostředků a diagnostiky', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KDS', N'Katedra dopravního stavitelství', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KE', N'Katedra elektrotechniky', 'FEI');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KEMCH', N'Katedra ekonomiky a managementu chemického a potravinářského průmyslu', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KFCH', N'Katedra fyzikální chemie', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KFI', N'Katedra filosofie', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KHVFR', N'Katedra humanitních věd FR', 'FR');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KCHTFR', N'Katedra chemické technologie FR', 'FR');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KID', N'Katedra informatiky v dopravě', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KIR', N'Katedra informatiky, managementu a radiologie', 'FZS');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KIT', N'Katedra informačních technologií', 'FEI');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KKO', N'Katedra klinických oborů', 'FZS');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KLKS', N'Katedra literární kultury a slavistiky', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KMF', N'Katedra matematiky a fyziky', 'FEI');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KMMCS', N'Katedra mechaniky, materiálů a částí strojů', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KOANCH', N'Katedra obecné a anorganické chemie', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KOS', N'Katedra ošetřovatelství', 'FZS');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KPF', N'Katedra polygrafie a fotofyziky', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KPZ', N'Katedra porodní asistence a zdravotně sociální práce', 'FZS');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KRE', N'Katedra religionistiky', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KRP', N'Katedra řízení procesů', 'FEI');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KST', N'Katedra softwarových technologií', 'FEI');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KSV', N'Katedra sociálních věd', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KTRD', N'Katedra technologie a řízení dopravy', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KTS', N'Katedra tělovýchovy a sportu', 'REK');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('KVV', N'Katedra věd o výchově', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('SLCHPL', N'Společná laboratoř chemie pevných látek', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UAFM', N'Ústav aplikované fyziky a matematiky', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UECHI', N'Ústav environmentálního a chemického inženýrství', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UENM', N'Ústav energetických materiálů', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UEV', N'Ústav ekonomických věd', 'FES');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UHV', N'Ústav historických věd', 'FF');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UCHTML', N'Ústav chemie a technologie makromolekulárních látek', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UK', N'Univerzitní knihovna', 'REK');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UMKM', N'Ústav matematiky a kvantitativních metod', 'FES');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UOCHT', N'Ústav organické chemie a technologie', 'FCH');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('UPEM', N'Ústav podnikové ekonomiky a managementu', 'FES');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('URBV', N'Ústav regionálních a bezpečnostních věd', 'FES');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('USII', N'Ústav systémového inženýrství a informatiky', 'FES');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('USSV', N'Ústav správních a sociálních věd', 'FES');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('VIP', N'Vzdělávací a informační pracoviště DFJP', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('VVCD', N'Výukové a výzkumné centrum v dopravě', 'DFJ');
+
+INSERT INTO KATEDRA (ZKRATKA_KATEDRY, NAZEV_KATEDRY, FAKULTA_ZKRATKA_FAKULTY) 
+VALUES ('VZC', N'Výzkumné centrum LN0A028', 'FCH');
+
+/*Insert Studijní Obor*/
+insert into stud_obor (nazev, zkratka, info) values ('Informační technologie','B2646',
+'Bakalářský prezenční studijní program Informační technologie je určen pro přípravu odborníků
+schopných realizovat a provozovat informační systémy a jejich moduly. Bakalářské studium je
+zaměřeno především na oblast informačních technologií  z hlediska metod zpracování informací,
+programovacích jazyků, výstavby a správy databází, aplikací systémové analýzy, správy a obsluhy
+počítačových sítí.');
+
+Insert into STUD_OBOR (NAZEV,ZKRATKA,INFO) values ('Řízení procesů','RIP','Cílem bakalářského oboru je připravit odborníky pro uplatnění v průmyslu v oblasti měření a automatického řízení, a to v projekci, výrobě, správě, diagnostice a údržbě měřicích, informačních, automatizačních, zabezpečovacích a obdobných zařízení. Absolvent má teoretický základ v matematice, fyzice a dalších vědách. V odborné oblasti pak získá znalosti v obecné elektrotechnice, programování a využívání komunikačních technologií. Jeho speciální znalosti se týkají teorie automatického řízení, modelování dynamických systémů, měření technologických veličin, zpracování signálů a programování řídicích aplikací. Funkční zaměření absolventů je předpokladem pro uplatnění v profesích: technický a provozní pracovník průmyslových, dopravních a jiných závodů');
+
+Insert into STUD_OBOR (NAZEV,ZKRATKA,INFO) values ('Komunikační a mikroprocesorová technika','KMT','Cílem studijního oboru je příprava vysokoškolsky vzdělaných odborníků s uplatněním v elektrotechnickém průmyslu, ale i v dalších odvětvích se zaměřením na aplikaci mikroprocesorů a počítačů při řízení, kontrole, ovládání i diagnostice elektronických systémů a zařízení. Absolvent má dobrý přehled v oblasti obecné elektrotechniky a elektroniky, teoretický základ v matematice a fyzice a dalších vědách. Jeho speciální znalosti se týkají přenosu a zpracování signálu a informací, dále pak aplikace mikroprocesorů a počítačů při řízení procesů. ');
+
+/*Sekvence a trigger STUD_PLAN*/
+CREATE SEQUENCE STUD_PLAN_SEQ 
+ START WITH 1 
+ INCREMENT BY 1;
+/
+ CREATE OR REPLACE TRIGGER TRIG_STUD_PLAN
+BEFORE INSERT ON STUD_PLAN
+FOR EACH ROW
+ WHEN (new.ID_PLANU IS NULL) 
+BEGIN
+  SELECT STUD_PLAN_SEQ.NEXTVAL
+  INTO :new.ID_PLANU
+  FROM dual;
+END;
+/
+
+/* Insert STUD_PLAN*/
+INSERT INTO STUD_PLAN (VERZE, STUD_OBOR_ID_OBORU) 
+VALUES (1, 1);
+
+INSERT INTO STUD_PLAN (VERZE, STUD_OBOR_ID_OBORU) 
+VALUES (1, 2);
+
+INSERT INTO STUD_PLAN (VERZE, STUD_OBOR_ID_OBORU) 
+VALUES (1, 3);
