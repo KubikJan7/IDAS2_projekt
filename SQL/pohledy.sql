e6069855e81cba6376b5adf65b257fff324197e2
Libor Selecký <st47137@upce.cz>
2018-12-18 12:40:17 +0100

    Přidán pohled Akce

--- SQL/pohledy.sql
+++ SQL/pohledy.sql
@@ -18,3 +18,17 @@ FROM fakulta fak JOIN katedra kat ON fak.zkratka_fakulty=kat.fakulta_zkratka_fak
 /*Studijní obory*/
 CREATE VIEW stud_obory_view AS SELECT id_oboru, zkratka, nazev, info, id_formy, nazev_formy
 FROM forma_vyuky f JOIN stud_obor s ON f.id_formy = s.forma_vyuky_id_formy;
+
+/*Rozvrhove akce*/
+create or replace view akce_view as select id_akce as , datum, cas_od as hodina, rozsah_hodin as doba, 
+rozvrhova_akce.kapacita as mist, uzivatel_id_uzivatele as id_uzivatel, titul_pred as titul, jmeno, prijmeni ,
+predmet_id_predmetu as id_predmet, predmet.NAZEV as predmet,
+zpusob_vyuky_id_nazvu as id_zpusobu, zpusob_vyuky.nazev as zpusob,
+ucebna_id_ucebny, nazev_ucebny as ucebna,
+id_tydne, tyden.nazev as tyden
+from rozvrhova_akce
+join uzivatel on id_uzivatele = uzivatel_id_uzivatele
+join predmet on id_predmetu = predmet_id_predmetu
+join zpusob_vyuky on id_nazvu = zpusob_vyuky_id_nazvu
+left join ucebna on id_ucebny = ucebna_id_ucebny
+left join tyden on id_tydne = tyden_id_tydne;
